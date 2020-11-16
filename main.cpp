b68ed356d1d01a12ec10fa522b08c515f7cb0d53
MykhailenkoAnton <mykhailencko2594@gmail.com>
2020-11-16 18:48:19 +0300

    ofstream

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
 int main()
 {
-    int a;
-    cout << "123!";
-    cin >> a;
+    const string wave = "C:\\Users\\mykha\\Documents\\base.txt";
+
+    ofstream fout;
+
+    fout.open(wave,ofstream::app);
+
+    if(!fout.is_open())
+    {
+        cout << "Error for open file";
+    }
+    else
+    {
+        cout << "Enter number: ";
+        string number;
+        getline(cin, number);
+        fout << "\n" << number;
+    }
+    fout.close();
+
     return 0;
 }
